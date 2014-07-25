86ca2cb4551531ef25d88ef33047436675d2e7f6
akshairajendran <akshai.rajendran@gmail.com>
2014-07-25 16:11:04 -0500

    login capabilities

--- get_alltweets.py
+++ get_alltweets.py
@@ -1,1 +1,25 @@
 __author__ = 'arajendran'
+import tweepy
+
+#open config file and create keys list from that
+f = open('config.txt','r')
+keys_list = [line[line.index(':')+1:len(line)].strip() for line in f]
+
+#create api keys
+consumer_key = keys_list[0]
+consumer_secret = keys_list[1]
+access_token = keys_list[2]
+access_token_secret = keys_list[3]
+
+#OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+#create api interface
+api = tweepy.API(auth)
+
+#############################################################
+#include this at the beginning of all twitter access scripts#
+#############################################################
+
+api.update_status("sup justin hello from python")
