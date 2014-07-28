9468d2635fdb8325b183fde209dcccbb66b0ca9b
akshairajendran <akshai.rajendran@gmail.com>
2014-07-27 21:40:48 -0500

    made main a function

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import random
 
 #infinite loop that sleeps for a random time between 4 and 8hrs
 #tweets on average every 6 hours or 4 times a day
-while True:
-    gen_tweet.tweet()
-    auto_follow.follow()
-    time.sleep(random.randint(14400,28800))
+def run():
+    while True:
+        gen_tweet.tweet()
+        auto_follow.follow()
+        time.sleep(random.randint(14400,28800))
 
 
